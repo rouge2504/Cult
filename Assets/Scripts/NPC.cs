a170af1c80381aedd7bdbc963d7e7f18833d9f5c
rouge2504 <rouge2504>
2022-11-24 12:52:04 -0600

    Add New scene and new gameplay

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NPC : BTAgent
+{
+    public enum Status { IDLE, WALKING};
+
+    public Status status = Status.IDLE;
+
+    public GameObject[] points;
+
+    public GameObject pointsContent;
+
+
+    new void Start()
+    {
+        base.Start();
+        agent = this.GetComponent<NavMeshAgent>();
+
+        tree = new BehaviourTree();
+
+        Sequence wander = new Sequence("Wander");
+
+        RSelector selectObject = new RSelector("Select Object To Move");
+
+        points = new GameObject[pointsContent.transform.childCount];
+
+        for (int i = 0; i < pointsContent.transform.childCount; i++)
+        {
+            points[i] = pointsContent.transform.GetChild(i).gameObject;
+        }
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Leaf gta = new Leaf("Go to: " + points[i].name, i, GoToPoint);
+            selectObject.AddChild(gta);
+        }
+
+
+
+
+        wander.AddChild(selectObject);
+
+        tree.AddChild(wander);
+
+        tree.PrintTree();
+
+    }
+
+    public Node.Status GoToPoint(int i)
+    {
+        if (!points[i].activeSelf) return Node.Status.FAILURE;
+        Node.Status s = GoToLocation(points[i].transform.position);
+
+        if (s == Node.Status.SUCCES)
+        {
+            //points[i].transform.parent = this.gameObject.transform;
+            //pickUp = points[i];
+        }
+
+        return s;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+
+}
---
+++ Assets/Scripts/Udemy/BTAgent.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BTAgent : MonoBehaviour
+{
+    public BehaviourTree tree;
+
+    public NavMeshAgent agent;
+
+    public enum ActionState { IDLE, WORKING};
+    public ActionState state = ActionState.IDLE;
+
+    public Node.Status treeStatus = Node.Status.RUNNING;
+
+    WaitForSeconds waitForSeconds;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+
+        tree = new BehaviourTree();
+
+        waitForSeconds = new WaitForSeconds(Random.Range(2, 5));
+        StartCoroutine(Behave());
+    }
+
+    public Node.Status GoToLocation(Vector3 destination)
+    {
+        float distantToTarget = Vector3.Distance(destination, this.transform.position);
+        if (state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }else if (Vector3.Distance(agent.pathEndPosition, destination) >= 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }else if  (distantToTarget  < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCES;
+        }
+
+        return Node.Status.RUNNING;
+
+    }
+
+    IEnumerator Behave()
+    {
+        while (true)
+        {
+            treeStatus = tree.Procces();
+            yield return waitForSeconds;
+        }
+    }
+
+}
